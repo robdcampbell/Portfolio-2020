5899e335290272a6610e7432d8a5f916da06f510
robdcampbell <rdeancampbell@gmail.com>
2021-01-04 23:32:24 -0500

    Hero section and nav done

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoplay loop muted src={Video} type="video/mp4"></VideoBg>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Rob Campbell.</HeroH1>
+        <HeroP>(Hi, hi, hi, there)</HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Projects {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
