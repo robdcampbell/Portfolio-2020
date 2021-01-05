d4acd3276a5d4c7eba7332d0f5844c759fe0d895
robdcampbell <rdeancampbell@gmail.com>
2021-01-04 20:34:26 -0500

    Styling done (for now). Movigng onto functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Sandbox from "./components/Sandbox";
 import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
-      <Sandbox />
     </Router>
   );
 }
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -14,21 +14,21 @@ export const Nav = styled.nav`
   font-size: 1rem;
   position: sticky;
   top: 0;
-  z-indez: 10;
+  z-index: 10;
 
   @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
   }
 `;
 export const NavbarContainer = styled.div`
-  display: flex; 
+  display: flex;
   justify-content: space-between;
   height: 80px;
   z-index: 1;
   width: 100%;
-  paddingL 0 24px;
+  padding: 0 24px;
   max-width: 1100 px;
-  `;
+`;
 
 // Named like this because the "LinkR" will act as a logo link, and use router to send you to another page.
 export const NavLogo = styled(LinkR)`
@@ -79,11 +79,39 @@ export const NavLinks = styled(LinkS)`
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding: 0 1 rem;
+  padding: 0 1rem;
   height: 100%;
   cursor: pointer;
 
   &.active {
     border-bottom: 3px solid #01bf71;
+  }
+`;
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 10px 22px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #fff;
+    color: #010606;
   }
 `;
